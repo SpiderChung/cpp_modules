7f1f8191068e7bf03a8985b932409618848501ad
schung <schung@student.21-school.ru>
2022-11-05 21:50:02 +0300

    cpp03_ex00_начало

--- cpp_03/ex01/ScavTrap.cpp
+++ cpp_03/ex01/ScavTrap.cpp
@@ -1,3 +1,30 @@
 #include "ScavTrap.hpp"
 
+ScavTrap::~ScavTrap() {
+
+}
+
+ScavTrap::ScavTrap() {
+
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap(name) {
+
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &other) {
+    return <#initializer#>;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &other) {
+
+}
+
+void ScavTrap::guardGate() {
+
+}
+
+void ScavTrap::attack(const std::string &target) {
+    ClapTrap::attack(target);
+}
 
--- cpp_03/ex01/ScavTrap.hpp
+++ cpp_03/ex01/ScavTrap.hpp
@@ -1,16 +1,21 @@
-
-
 #ifndef SCAVTRAP_HPP
 #define SCAVTRAP_HPP
 
+#include "ClapTrap.hpp"
 
-class ScavTrap {
+class ScavTrap : public ClapTrap {
 private:
-
+    static const int _maxHits = 100;
+    int guard;
 public:
-
+    ScavTrap();
+    ~ScavTrap();
+    ScavTrap(std::string name);
+    ScavTrap(ScavTrap const &other);
+    ScavTrap &operator=(ScavTrap const &other);
     void guardGate();
-};
 
+    void attack(const std::string &target);
+};
 
 #endif //SCAVTRAP_HPP
